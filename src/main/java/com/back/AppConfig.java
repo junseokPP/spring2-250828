fd76294085c19bf7e3e571c274300249ea971673
junseokPP <pjs981130@gmail.com>
2025-09-07 16:23:45 +0900

    4강 : Service사용

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,14 +1,13 @@
 package com.back;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-
-    @Bean
-    public PersonService getPersonService(){
-        return new PersonService();
-    }
+//
+//    @Bean
+//    public PersonService getPersonService(){
+//        return new PersonService();
+//    }
 
 }
